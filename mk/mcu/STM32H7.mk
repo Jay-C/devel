871791b943dd1583c86a872bf01d48e49ef94571
Petri Mattila <petri.mattila@unipart.com>
2024-06-14 12:50:29 +0100

    RTFL: Remove TRANSPONDER

--- mk/mcu/STM32H7.mk
+++ mk/mcu/STM32H7.mk
@@ -327,7 +327,6 @@ MCU_COMMON_SRC = \
             drivers/stm32/system_stm32h7xx.c \
             drivers/stm32/timer_hal.c \
             drivers/stm32/timer_stm32h7xx.c \
-            drivers/stm32/transponder_ir_io_hal.c \
             drivers/stm32/camera_control.c \
             startup/system_stm32h7xx.c
 
