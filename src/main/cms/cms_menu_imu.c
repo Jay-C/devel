aa28653eb72a8003df2c9cbd1793c3f5224e5551
Petri Mattila <petri.mattila@unipart.com>
2024-06-17 13:37:59 +0100

    RTFL: Remove TPA_MODE

--- src/main/cms/cms_menu_imu.c
+++ src/main/cms/cms_menu_imu.c
@@ -258,11 +258,6 @@ static uint8_t cmsx_simplified_dterm_filter;
 static uint8_t cmsx_simplified_dterm_filter_multiplier;
 static uint8_t cmsx_simplified_gyro_filter;
 static uint8_t cmsx_simplified_gyro_filter_multiplier;
-static uint8_t cmsx_tpa_rate;
-static uint16_t cmsx_tpa_breakpoint;
-static uint8_t cmsx_tpa_low_rate;
-static uint16_t cmsx_tpa_low_breakpoint;
-static uint8_t cmsx_tpa_low_always;
 
 static const void *cmsx_simplifiedTuningOnEnter(displayPort_t *pDisp)
 {
@@ -458,12 +453,6 @@ static uint8_t cmsx_feedforward_smooth_factor;
 static uint8_t cmsx_feedforward_jitter_factor;
 #endif
 
-static uint8_t cmsx_tpa_rate;
-static uint16_t cmsx_tpa_breakpoint;
-static uint8_t cmsx_tpa_low_rate;
-static uint16_t cmsx_tpa_low_breakpoint;
-static uint8_t cmsx_tpa_low_always;
-
 static const void *cmsx_profileOtherOnEnter(displayPort_t *pDisp)
 {
     UNUSED(pDisp);
@@ -502,11 +491,6 @@ static const void *cmsx_profileOtherOnEnter(displayPort_t *pDisp)
 #ifdef USE_BATTERY_VOLTAGE_SAG_COMPENSATION
     cmsx_vbat_sag_compensation = pidProfile->vbat_sag_compensation;
 #endif
-    cmsx_tpa_rate = pidProfile->tpa_rate;
-    cmsx_tpa_breakpoint = pidProfile->tpa_breakpoint;
-    cmsx_tpa_low_rate = pidProfile->tpa_low_rate;
-    cmsx_tpa_low_breakpoint = pidProfile->tpa_low_breakpoint;
-    cmsx_tpa_low_always = pidProfile->tpa_low_always;
 
     return NULL;
 }
@@ -549,11 +533,6 @@ static const void *cmsx_profileOtherOnExit(displayPort_t *pDisp, const OSD_Entry
 #ifdef USE_BATTERY_VOLTAGE_SAG_COMPENSATION
     pidProfile->vbat_sag_compensation = cmsx_vbat_sag_compensation;
 #endif
-    pidProfile->tpa_rate = cmsx_tpa_rate;
-    pidProfile->tpa_breakpoint = cmsx_tpa_breakpoint;
-    pidProfile->tpa_low_rate = cmsx_tpa_low_rate;
-    pidProfile->tpa_low_breakpoint = cmsx_tpa_low_breakpoint;
-    pidProfile->tpa_low_always = cmsx_tpa_low_always;
 
     initEscEndpoints();
     return NULL;
@@ -589,12 +568,6 @@ static const OSD_Entry cmsx_menuProfileOtherEntries[] = {
 #ifdef USE_BATTERY_VOLTAGE_SAG_COMPENSATION
     { "VBAT_SAG_COMP", OME_UINT8,  NULL, &(OSD_UINT8_t) { &cmsx_vbat_sag_compensation, 0, 150, 1 } },
 #endif
-
-    { "TPA RATE",      OME_FLOAT,  NULL, &(OSD_FLOAT_t) { &cmsx_tpa_rate, 0, 100, 1, 10} },
-    { "TPA BRKPT",     OME_UINT16, NULL, &(OSD_UINT16_t){ &cmsx_tpa_breakpoint, 1000, 2000, 10} },
-    { "TPA LOW RATE",  OME_UINT8,  NULL, &(OSD_UINT8_t) { &cmsx_tpa_low_rate, 0, 100, 1} },
-    { "TPA LOW BRKPT", OME_UINT16, NULL, &(OSD_UINT16_t){ &cmsx_tpa_low_breakpoint, 1000, 2000, 10} },
-    { "TPA LOW ALWYS", OME_Bool,   NULL, &cmsx_tpa_low_always },
 
     { "BACK", OME_Back, NULL, NULL },
     { NULL, OME_END, NULL, NULL}
