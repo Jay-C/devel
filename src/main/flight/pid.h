42375ac18a4dd1e75bca1009860b748a9ea5d904
Petri Mattila <petri.t.j.mattila@gmail.com>
2024-07-11 23:53:01 +0100

    RTFL: Remove EZ_LANDING

--- src/main/flight/pid.h
+++ src/main/flight/pid.h
@@ -128,9 +128,6 @@ typedef struct pidProfile_s {
     uint8_t angle_earth_ref;                // Control amount of "co-ordination" from yaw into roll while pitched forward in angle mode
     uint16_t horizon_delay_ms;              // delay when Horizon Strength increases, 50 = 500ms time constant
 
-    uint8_t ez_landing_threshold;           // Threshold stick position below which motor output is limited
-    uint8_t ez_landing_limit;               // Maximum motor output when all sticks centred and throttle zero
-    uint8_t ez_landing_speed;               // Speed below which motor output is limited
 } pidProfile_t;
 
 PG_DECLARE_ARRAY(pidProfile_t, PID_PROFILE_COUNT, pidProfiles);
