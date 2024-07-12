aa2c7e11c0180fc388a1b8dbf06a15f3c9725821
Petri Mattila <petri.t.j.mattila@gmail.com>
2024-07-12 09:25:28 +0100

    RTFL: Remove AUTO_PROFILE_CELL_COUNT

--- src/main/sensors/battery.c
+++ src/main/sensors/battery.c
@@ -205,10 +205,6 @@ void batteryUpdatePresence(void)
                 cells = MAX_AUTO_DETECT_CELL_COUNT;
             }
             batteryCellCount = cells;
-
-            if (!ARMING_FLAG(ARMED)) {
-                changePidProfileFromCellCount(batteryCellCount);
-            }
         }
 #ifdef USE_RPM_LIMIT
         mixerResetRpmLimiter();
