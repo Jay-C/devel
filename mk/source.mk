dd18a38aedfdd21f906de520c91f48e604f1941d
Petri Mattila <petri.t.j.mattila@gmail.com>
2024-07-12 10:48:35 +0100

    RTFL: Remove USE_GPS_LAP_TIMER

--- mk/source.mk
+++ mk/source.mk
@@ -11,7 +11,6 @@ PG_SRC = \
             pg/dyn_notch.c \
             pg/flash.c \
             pg/gps.c \
-            pg/gps_lap_timer.c \
             pg/gps_rescue.c \
             pg/gyrodev.c \
             pg/max7456.c \
@@ -151,7 +150,6 @@ COMMON_SRC = \
             flight/position.c \
             flight/failsafe.c \
             flight/gps_rescue.c \
-            fc/gps_lap_timer.c \
             flight/dyn_notch_filter.c \
             flight/imu.c \
             flight/mixer.c \
@@ -203,7 +201,6 @@ COMMON_SRC = \
             cms/cms_menu_failsafe.c \
             cms/cms_menu_firmware.c \
             cms/cms_menu_gps_rescue.c \
-            cms/cms_menu_gps_lap_timer.c \
             cms/cms_menu_imu.c \
             cms/cms_menu_ledstrip.c \
             cms/cms_menu_main.c \
@@ -507,7 +504,6 @@ SIZE_OPTIMISED_SRC := $(SIZE_OPTIMISED_SRC) \
             cms/cms_menu_failsafe.c \
             cms/cms_menu_firmware.c \
             cms/cms_menu_gps_rescue.c \
-            cms/cms_menu_gps_lap_timer.c \
             cms/cms_menu_imu.c \
             cms/cms_menu_ledstrip.c \
             cms/cms_menu_main.c \
