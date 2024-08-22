2d5d76282b29f90c3508368fcd1ec48ebdcd2b53
Petri Mattila <petri.t.j.mattila@gmail.com>
2024-08-22 23:15:56 +0100

    RTFL: Remove blackbox motor test mode

--- src/main/cli/settings.c
+++ src/main/cli/settings.c
@@ -225,7 +225,7 @@ static const char * const lookupTableBlackboxDevice[] = {
 };
 
 static const char * const lookupTableBlackboxMode[] = {
-    "NORMAL", "MOTOR_TEST", "ALWAYS"
+    "NORMAL",
 };
 
 static const char * const lookupTableBlackboxSampleRate[] = {
