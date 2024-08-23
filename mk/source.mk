a13a879c9f5515addcb7fc508c53f852a40dd054
Petri Mattila <petri.t.j.mattila@gmail.com>
2024-08-23 13:13:21 +0100

    RTFL: Remove USE_SIMPLIFIED_TUNING

--- mk/source.mk
+++ mk/source.mk
@@ -79,7 +79,6 @@ COMMON_SRC = \
             config/config_eeprom.c \
             config/config_streamer.c \
             config/feature.c \
-            config/simplified_tuning.c \
             cli/cli.c \
             cli/settings.c \
             config/config.c \
@@ -472,7 +471,6 @@ SIZE_OPTIMISED_SRC += \
             config/config_eeprom.c \
             config/feature.c \
             config/config_streamer.c \
-            config/simplified_tuning.c \
             io/dashboard.c \
             io/serial.c \
             io/serial_4way.c \
