08485946818e18ba20e07b80ba2c4ff199e14d84
Petri Mattila <petri.t.j.mattila@gmail.com>
2024-08-24 22:57:42 +0100

    RTFL: Move gyroConfig to pg/gyro

--- mk/source.mk
+++ mk/source.mk
@@ -22,6 +22,7 @@ PG_SRC = \
             pg/flash.c \
             pg/gps.c \
             pg/gps_rescue.c \
+            pg/gyro.c \
             pg/gyrodev.c \
             pg/imu.c \
             pg/max7456.c \
